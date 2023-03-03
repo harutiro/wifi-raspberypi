17a2df40dfc215117ae5b590cb2f7e5f5b01b33e
harutiro <harutiro2027@outlook.jp>
2023-03-03 22:42:19 +0900

    update:serverの作成

--- server/my_server.py
+++ server/my_server.py
@@ -1,14 +1,34 @@
 from flask import Flask , request , render_template
 from flask_restx import Resource, Api ,fields
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)  # Flask に Flask-RESTX を導入
+CORS(app)
 
 resource_fields = api.model("Json Bodyです", {
     "latitude": fields.Float,
     "longitude": fields.Float
 })
 
+@api.route('/emergency' ,methods=['get'])
+class emergency(Resource):
+
+    def get(self):
+        return {
+            "status":"ok"
+        }
+    
+@api.route('/emergency/no' ,methods=['get'])
+class emergency(Resource):
+    
+    def get(self):
+        return {
+            "status":"no",
+            "latitude":100.0,
+            "longitude":70.0
+
+        }
 
 @api.route('/location', methods=['post'])
 class Location(Resource):
@@ -26,4 +46,4 @@ class Location(Resource):
 
 ## おまじない
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
